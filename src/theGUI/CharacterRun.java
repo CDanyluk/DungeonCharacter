1b920fe43739c2f524be055da5720af0a3a635e3
CDanyluk <CD@Danyluk.com>
2017-03-16 18:05:41 -0500

    Finished CharacterSheet
    Added a class for future GUI linking purposes

---
+++ src/theGUI/CharacterRun.java
@@ -0,0 +1,25 @@
+package theGUI;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class CharacterRun extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("CharacterSheet.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
