21e917477c529331f5f9190642625ab7cd93c10d
CDanyluk <CD@Danyluk.com>
2017-04-09 19:49:28 -0500

    bam
    dhj

--- src/theGUI/SearchController.java
+++ src/theGUI/SearchController.java
@@ -51,6 +51,7 @@ public class SearchController {
 
 				CharacterController second = (CharacterController)loader.getController();
 				second.name.setText(createName.getText());
+				second.initialize();
 
 				Stage secondStage = new Stage();
 				Scene scene = new Scene(root);
