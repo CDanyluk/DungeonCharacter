c659bf5764728edce26a3185499794470e8f5784
CDanyluk <CD@Danyluk.com>
2017-04-11 12:50:22 -0500

    changes for final code

--- src/Classes/Export.java
+++ src/Classes/Export.java
@@ -45,10 +45,10 @@ public class Export {
 			if (destination != null) {
 				fileWriter = new FileWriter(destination);
 				fileWriter.write(character.getName() + String.format("%n"));
-				writeSkills();
-				writeAttributes();
- 				writeStatistics();
-				writeMisc();
+				writeSkills(fileWriter);
+				writeAttributes(fileWriter);
+ 				writeStatistics(fileWriter);
+				writeMisc(fileWriter);
 				fileWriter.close();
 			}
 		} catch (IOException ex) {
@@ -56,39 +56,39 @@ public class Export {
 		}
 	}
 	// TODO: Write functions like this one to write Attributes, Miscellaneous, and Statistics
-	private void writeSkills() throws IOException {
-		fileWriter.write("SKILLS" + String.format("%n"));
+	public void writeSkills(FileWriter fw) throws IOException {
+		fw.write("SKILLS" + String.format("%n"));
 		for (Skills s : Skills.values()) {
-			fileWriter.write("\t" + s.toString() + ": ");
-			fileWriter.write("\t" + character.charSkills.get(s) + ";");
-			fileWriter.write(String.format("%n"));
+			fw.write("\t" + s.toString() + ": ");
+			fw.write("\t" + character.charSkills.get(s) + ";");
+			fw.write(String.format("%n"));
 		}
 	}
 // These will be null while testing; skills are automatically set to 0 in initialize but others are not
-	private void writeAttributes() throws IOException {
-		fileWriter.write("ATTRIBUTES" + String.format("%n"));
+	public void writeAttributes(FileWriter fw) throws IOException {
+		fw.write("ATTRIBUTES" + String.format("%n"));
 		for (Attributes a : Attributes.values()) {
-			fileWriter.write("\t" + a.toString() + ": ");
-			fileWriter.write("\t" + character.charAttri.get(a) + "; ");
-			fileWriter.write(String.format("%n"));
+			fw.write("\t" + a.toString() + ": ");
+			fw.write("\t" + character.charAttri.get(a) + "; ");
+			fw.write(String.format("%n"));
 		}
 	}
 
-	private void writeStatistics() throws IOException {
-		fileWriter.write("STATISTICS" + String.format("%n"));
+	public void writeStatistics(FileWriter fw) throws IOException {
+		fw.write("STATISTICS" + String.format("%n"));
 		for (Statistics s : Statistics.values()) {
-			fileWriter.write("\t" + s.toString() + ": ");
-			fileWriter.write("\t" + character.charStats.get(s) + "; ");
-			fileWriter.write(String.format("%n"));
+			fw.write("\t" + s.toString() + ": ");
+			fw.write("\t" + character.charStats.get(s) + "; ");
+			fw.write(String.format("%n"));
 		}
 	}
 
-	private void writeMisc() throws IOException {
-		fileWriter.write("MISCELLANEOUS" + String.format("%n"));
+	public void writeMisc(FileWriter fw) throws IOException {
+		fw.write("MISCELLANEOUS" + String.format("%n"));
 		for (Miscellaneous m : Miscellaneous.values()) {
-			fileWriter.write("\t" + m.toString() + ": ");
-			fileWriter.write("\t" + character.charExtra.get(m) + "; ");
-			fileWriter.write(String.format("%n"));
+			fw.write("\t" + m.toString() + ": ");
+			fw.write("\t" + character.charExtra.get(m) + "; ");
+			fw.write(String.format("%n"));
 		}
 	}
 
